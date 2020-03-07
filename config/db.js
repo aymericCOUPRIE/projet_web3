4e684463fd42e04801babb207844a6e4b074902c
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-03-07 12:38:40 +0100

    Modif avec createPool

--- config/db.js
+++ config/db.js
@@ -1,5 +1,7 @@
 var mysql = require('mysql');
 
+
+/*
 var connexion = mysql.createConnection({
     database: 'heroku_6eb2a7bfc1b31b2',
     host: 'eu-cdbr-west-02.cleardb.net',
@@ -11,13 +13,22 @@ var connexion = mysql.createConnection({
 connexion.connect( function (err ) {
     console.log(err);
 });
+*/
 
-/*
+
+var connexion = mysql.createPool({
+    database: 'heroku_6eb2a7bfc1b31b2',
+    host: 'eu-cdbr-west-02.cleardb.net',
+    user: 'bbab736053d516',
+    password: '68bd7814'
+});
+
+
 connexion.getConnection((err, connect) => {
     if(err)
         console.error("Something went wrong connecting to the database ...");
     if(connect)
         connect.release();
 });
-*/
+
 module.exports = connexion;
