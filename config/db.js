6c940f36feaa26fa3a8a12ec1af12b1833fa2e69
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-03-07 11:59:41 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,10 +1,10 @@
 var mysql = require('mysql');
 
 var connexion = mysql.createConnection({
-    database: 'heroku_6eb2a7bfc1b31b2',
-    host: 'eu-cdbr-west-02.cleardb.net',
-    user: 'bbab736053d516',
-    password: '68bd7814'
+    database: 'nodeJSDb',
+    host: 'localhost',
+    user: 'root',
+    password: ''
 });
 
 connexion.connect(function (err) {
