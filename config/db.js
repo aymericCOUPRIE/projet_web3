21241b0b12a5bc56a9442d8dd8ca455c42d2c540
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-03-08 14:35:15 +0100

    update db

--- config/db.js
+++ config/db.js
@@ -15,21 +15,3 @@ connexion.connect(function (err) {
 });
 
 module.exports = connexion;
-
-
-/*
-var connexion = mysql.createPool({
-    database: 'heroku_6eb2a7bfc1b31b2',
-    host: 'eu-cdbr-west-02.cleardb.net',
-    user: 'bbab736053d516',
-    password: '68bd7814'
-});
-
-
-connexion.getConnection((err, connect) => {
-    if(err)
-        console.error("Something went wrong connecting to the database ...");
-    if(connect)
-        connect.release();
-});
-*/
