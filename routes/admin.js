9fe4fc6e337dff163b4362335a215c4a8bf986db
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-03-08 19:28:58 +0100

    test ajout image

--- routes/admin.js
+++ routes/admin.js
@@ -4,6 +4,16 @@ var express = require('express');
 var router = express.Router();
 var admin = require('../controllers/adminCtrl.js');
 
+var multer = require("multer");
+var multerGoogleStorage = require('multer-google-storage');
+
+var uploadHandler = multer({
+    storage: multerGoogleStorage.storageEngine({
+        keyFilename: "./platinum-avenue-270020-f889a4f9b841.json",
+        projectId: 'platinum-avenue-270020',
+        bucket: 'projetweb'
+    })
+});
 
 router.use( function (req,res, next) {
     admin.isAdmin(req, function (result) {
@@ -22,5 +32,12 @@ router.get('/modifierSite/*', admin.afficherModification);
 router.post('/ajoutSite', admin.ajoutSite);
 router.delete('/supprimerSite/?*', admin.deleteCarriere);
 router.put('/modifierSite/?*', admin.modifierSite);
+
+//router.get('/upload', home.formImageDisplay);
+router.post('/uploadPhoto', uploadHandler.any(), function (req, res) {
+    //admin.insertImage(req.files[0].path);
+    res.redirect('/');
+//    console.log(req.files[0].path);
+});
 
 module.exports = router;
--- routes/home.js
+++ routes/home.js
@@ -4,6 +4,7 @@ var home = require('../controllers/usersCtrl');
 var multer = require("multer");
 var multerGoogleStorage = require('multer-google-storage');
 
+/*
 var uploadHandler = multer({
     storage: multerGoogleStorage.storageEngine({
         keyFilename: "./platinum-avenue-270020-f889a4f9b841.json",
@@ -11,17 +12,19 @@ var uploadHandler = multer({
         bucket: 'projetweb'
     })
 });
+*/
 
 router.get('/', home.accueil);
 router.get('/home', home.accueil);
 router.get('/maps', home.mapsDisplay);
 router.get('/deconnexion', home.deconnexion);
 
-
+/*
 router.get('/upload', home.formImageDisplay);
 
 router.post('/upload', uploadHandler.any(), function (req, res) {
     res.json(req.files);
 });
+*/
 
 module.exports = router;
