24a9fefc8a57151f0c3a41871387884a59b31d18
aymericCOUPRIE <43545632+aymericCOUPRIE@users.noreply.github.com>
2020-03-08 23:25:26 +0100

    erreur db

--- config/db.js
+++ config/db.js
@@ -1,30 +1,16 @@
 var mysql = require('mysql');
 
+
 /*
-var connexion = mysql.createConnection({
+var connexion = mysql.createPool({
     database: 'nodeJSDb',
     host: 'localhost',
     user: 'root',
     password: ''
 });
 
-connexion.connect(function (err) {
-    if (err)
-        throw err;
-
-    console.log("Connexion effectuée");
-});
 */
-
-
-
-var connexion = mysql.createPool({
-    database: 'heroku_6eb2a7bfc1b31b2',
-    host: 'eu-cdbr-west-02.cleardb.net',
-    user: 'bbab736053d516',
-    password: '68bd7814'
-});
-
+const connexion = mysql.createPool(process.env.CLEARDB_DATABASE_URL);
 
 connexion.getConnection((err, connect) => {
     if(err)
